679c7c6154f310e0a7fafe2dc7af53fe52ac22e7
zenklot <zenklot11@gmail.com>
2022-11-04 06:37:46 +0700

    :sparkles: : Get monster from API

--- src/App.js
+++ src/App.js
@@ -19,7 +19,12 @@ class App extends Component {
         }
       ]
     }
+  }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => { return {"monsters" : users}}, () => console.log(this.state.monsters )))
   }
 
   render() {
