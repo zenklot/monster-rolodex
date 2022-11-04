494e78f19aa1bbb3394cf31fa1535199e1774453
zenklot <zenklot11@gmail.com>
2022-11-04 07:39:25 +0700

    :sparkles: : Add filter search

--- src/App.js
+++ src/App.js
@@ -1,44 +1,61 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      "monsters" : [
-        {
-          "id": 1,
-          "name": "One"
-        },
-        {
-          "id" : 2,
-          "name": "Two"
-        }
-      ]
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState(() => { return {"monsters" : users}}, () => console.log(this.state.monsters )))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state.monsters)
+        )
+      );
   }
 
   render() {
+
+    const filterMonsters = this.state.monsters.filter((monster) => monster.name
+      .toLowerCase()
+      .includes(this.state.searchField)
+  )
+
     return (
-      <div className='App'>
-        {
-          this.state.monsters.map(monster => {
-            return ( <div key={monster.id}>
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search monsters..."
+          className="search-box"
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return {
+                searchField
+              };
+            });
+          }}
+        />
+        {filterMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
               <h1>{monster.name}</h1>
-            </div>)
-          }) 
-        }
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
